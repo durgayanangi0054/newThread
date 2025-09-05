598d850471f82496b4b6c7bb9bef0d078cf14f88
durgayanangi0054 <durgayanangi@gmail.com>
2025-09-05 19:40:50 +0530

    removed unused imports in hook.java

--- src/test/java/utils/Hooks.java
+++ src/test/java/utils/Hooks.java
@@ -5,8 +5,6 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Parameters;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.MediaEntityBuilder;
