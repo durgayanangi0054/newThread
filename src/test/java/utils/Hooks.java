73dc3903529dbdbaaee9c315347b32bae6eafea1
durgayanangi0054 <durgayanangi@gmail.com>
2025-09-06 20:22:05 +0530

    commit

--- src/test/java/utils/Hooks.java
+++ src/test/java/utils/Hooks.java
@@ -66,7 +66,7 @@ public class Hooks {
 			} 
 			else if (scenario.getStatus() == Status.SKIPPED) 
 			{
-				test.get().skip("Scenario skipped");
+				test.get().skip("Scenario skippeed");
 			}
 
 		} 
